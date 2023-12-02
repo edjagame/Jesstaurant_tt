70fafe6d607f635c377d5ddca517cd94d80da59b
edjagame <ejgmejilla@gmail.com>
2023-12-02 19:31:24 +0800

    Added Passenger model and started on ticket model

--- tirian_trains/tickets/admin.py
+++ tirian_trains/tickets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Passenger, Ticket
 
-# Register your models here.
+class PassengerAdmin(admin.ModelAdmin):
+    model = Passenger
+
+
+class TicketAdmin(admin.ModelAdmin):
+    model = Ticket
+
+
+admin.site.register(Passenger, PassengerAdmin)
+admin.site.register(Ticket, TicketAdmin)
--- tirian_trains/tickets/models.py
+++ tirian_trains/tickets/models.py
@@ -1,3 +1,27 @@
+from datetime import datetime
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Passenger(models.Model):
+    customer_id = models.IntegerField(primary_key=True, default=0000)
+    last_name = models.CharField(max_length=100, default="Doe")
+    given_name = models.CharField(max_length=100, default="John")
+    middle_initial = models.CharField(max_length=2, blank=True)
+    birth_date = models.DateField(auto_now_add=True)
+    gender = models.CharField(max_length=100, blank=True)
+
+    def get_age(self):
+        return int((datetime.date.today().year - self.birth_date.date().year) / 365.25)
+
+    def __str__(self):
+       return f"{self.customer_id} - {self.last_name}, {self.given_name} {self.middle_initial}" 
+
+    def get_absolute_url(self):
+        return reverse('tickets:passenger-details', kwargs={'pk':self.pk})
+
+
+#Still needs total cost from scheduled ticket
+class Ticket(models.Model):
+    ticket_id = models.IntegerField(primary_key=True, default=00000)
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+    # trip = models.ForeignKey("scheduled_trips.Trip", on_delete=models.CASCADE)
